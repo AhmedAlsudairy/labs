e7e39be221ce6624ae37364a73ca48f6ca4d837f
Ahmed <gfg59204@gmail.com>
2024-12-25 08:14:51 +0400

    add rrecords counters

--- components/tables/description-modal.tsx
+++ components/tables/description-modal.tsx
@@ -23,13 +23,15 @@ export function DescriptionModal({
 }: DescriptionModalProps) {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="max-w-[90vw] w-full md:max-w-[600px] max-h-[90vh] flex flex-col">
-        <DialogHeader>
-          <DialogTitle className="text-xl font-semibold">{title}</DialogTitle>
+      <DialogContent className="max-w-[95vw] w-full sm:max-w-[500px] md:max-w-[600px] lg:max-w-[700px] max-h-[90vh] flex flex-col gap-4">
+        <DialogHeader className="px-6 pt-6">
+          <DialogTitle className="text-xl font-semibold text-center sm:text-left">{title}</DialogTitle>
         </DialogHeader>
-        <ScrollArea className="flex-1 p-6">
+        <ScrollArea className="flex-1 px-6 pb-6">
           <div className="prose dark:prose-invert max-w-none">
-            <div className="whitespace-pre-wrap">{description}</div>
+            <div className="whitespace-pre-wrap text-sm sm:text-base leading-relaxed min-h-[100px] max-h-[60vh] overflow-y-auto">
+              {description}
+            </div>
           </div>
         </ScrollArea>
       </DialogContent>
